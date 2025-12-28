c790ec4f31dc6e43fd7fa7b3b27d326afa0cecc7
Mustansir Muhammad <tp078682@mail.apu.edu.my>
2025-12-28 16:55:31 +0800

    save scripts and authentication implemented

---
+++ app/auth/page.tsx
@@ -0,0 +1,140 @@
+'use client'
+
+import { useState } from 'react'
+import { supabase } from '../utils/supabase'
+import { useRouter } from 'next/navigation'; // App Router
+import Image from 'next/image';
+
+export default function AuthPage() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  const [organization, setOrginization] = useState('')
+  const router = useRouter();
+
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [signUpFlag, setSignUpFlag] = useState(false)
+
+  const signIn = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    setError('')
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    })
+
+    if (error) setError(error.message)
+    setLoading(false)
+
+    // If there is no error means successful signIn then go to dashboard
+    if (!error) router.replace("/home");
+}
+
+  const signUp = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    setError('')
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password
+    })
+
+    if (error) setError(error.message)
+    setLoading(false)
+    setSignUpFlag(false)
+  }
+
+
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+
+      {/* Image on top */}
+      <Image
+        src="/layar.png"
+        width={240}
+        height={240}
+        alt="Logo"
+        className="mb-4"
+        priority
+      />
+
+      <div className="bg-white w-[360px] p-6 rounded-2xl shadow-lg">
+
+        <h1 className="text-2xl font-semibold text-center mb-6">
+          {signUpFlag ? "Sign Up" : "Sign In "}
+        </h1>
+
+        <form onSubmit={signUpFlag ? signUp : signIn} className="space-y-4">
+
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+
+          {signUpFlag && (
+            <>
+
+              <input
+                type="text"
+                placeholder="Name"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                required
+              />
+
+              <input
+                type="text"
+                placeholder="Organization"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+                value={organization}
+                onChange={e => setOrginization(e.target.value)}
+                required
+              />
+
+            </>
+          )}
+
+          {error && (
+            <p className="text-sm text-red-500">{error}</p>
+          )}
+
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition disabled:opacity-50 cursor-pointer"
+          >
+            {signUpFlag ? loading ? 'Signing Up…' : 'Sign Up' : loading ? 'Signing in…' : 'Sign In'}
+          </button>
+
+          <p className="text-sm text-black-500 underline text-center cursor-pointer"
+          onClick={() => setSignUpFlag(!signUpFlag)}
+          >
+            {signUpFlag ? 'Sign In' : 'Sign Up Now'}
+          </p>
+
+        </form>
+      </div>
+
+    </div>
+  )
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,6 +3,10 @@
 import { useEffect, useState } from "react";
 import { MinusCircleIcon, PlusCircleIcon, TrashIcon, ArrowsUpDownIcon  } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import { useRouter } from 'next/navigation'; // App Router
+
+import { supabase } from '../utils/supabase'
+import { User } from '@supabase/supabase-js';
 
 export default function EditableTable() {
 
@@ -30,10 +34,10 @@ export default function EditableTable() {
     estimatedTime: number;
   };
 
-  const [data, setData] = useState<SceneRow[]>([]);
+  const router = useRouter();
+  const [scenesData, setScenesData] = useState<SceneRow[]>([]);
   const [loading, setLoading] = useState(true);
   const [file, setFile] = useState<File | null>(null);
-
   const [modalConfig, setModalConfig] = useState({
     display: false,
     name: "",
@@ -41,25 +45,44 @@ export default function EditableTable() {
     scene: 0
   });
   const [itemToAdd, setItemToAdd] = useState("");
+  const [user, setUser] = useState<User | null>(null);
+  const [savedScripts, setSavedScripts] = useState<{ id: string; script_name: string }[]>([]);  
+  const [currentScriptId, setCurrentScriptId] = useState<number | null>(null);
 
-  // // To be Used for Testing....
-  // useEffect(()=>{
-  //   fetch("/api/getSchedule", {
-  //     method: "GET",
-  //   })
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     console.log(data);
-  //     setData(data.scenesData);
-  //     setLoading(false);
-  //   })
-  //   .catch(err => console.error(err));
-  // },[])
-  
+  useEffect(() => {
+    async function checkUser() {
+      const { data: { user }, error } = await supabase.auth.getUser();
+      if (error || !user) {
+        router.replace('/auth');
+      } else {
+        setUser(user);
+      }
+    }
+    checkUser();
+  },[router])
+
+
+  // Runs only on refresh
+  useEffect(() => {
+    if (!user) return; // needed to make sure it always loads correctly as user might not be avlbl initially
+    async function getScripts(){
+      const {data, error} = await supabase
+        .from('Scripts')
+        .select('id, script_name')
+        .eq('uid', user?.id);
+     
+        if (!error) {
+          setSavedScripts(data)
+        };
+
+    }
+    getScripts();
+  }, [user])
+
 
   // When User Wants to Change Some Stuff
   const handleChange = ( scene_number: number, field: keyof SceneRow, value: string) => {
-    setData(prev =>
+    setScenesData(prev =>
       prev.map(row => (row.scene_number === scene_number ? { ...row, [field]: value } : row))
     );
   };
@@ -71,7 +94,7 @@ export default function EditableTable() {
     index: number,
     value: string
   ) => {      
-    setData(prev =>
+    setScenesData(prev =>
       prev.map(row => {
         if (row.scene_number !== scene_number) return row;
 
@@ -90,16 +113,6 @@ export default function EditableTable() {
   /*
     WHY WE USE FILTER/SPREAD INSTEAD OF PUSH/SPLICE:
 
-    ❌ fBAD - push() changes the SAME array:
-    const arr = [1, 2, 3];
-    arr.push(4);  // arr is now [1, 2, 3, 4] but it's the SAME array object
-    // React: "Same array? No change detected. Don't re-render."
-
-    ✅ GOOD - filter/spread creates a NEW array:
-    const arr = [1, 2, 3];
-    const newArr = [...arr, 4];  // This is a BRAND NEW array [1, 2, 3, 4]
-    // React: "Different array? Change detected! Re-render the UI."
-
     SIMPLE RULE:
     - push, splice, pop = modify original (React won't see changes)
     - filter, map, [...arr] = create new copy (React sees changes and updates screen)
@@ -109,7 +122,7 @@ export default function EditableTable() {
   */
 
   const removeItem = (scene_number: number, field: keyof SceneRow, value: string) => {
-    setData(prev =>
+    setScenesData(prev =>
       prev.map(row => 
         row.scene_number === scene_number 
           ? { 
@@ -124,7 +137,7 @@ export default function EditableTable() {
   };
 
   const addItem = (scene_number: number, field: keyof SceneRow, value: string) => {
-    setData(prev =>
+    setScenesData(prev =>
       prev.map(row => 
         row.scene_number === scene_number 
           ? { 
@@ -138,9 +151,49 @@ export default function EditableTable() {
     );
   };
 
+  // save new scripts and updated ones too
+  const saveScript = async () =>{
+    if (currentScriptId){
+      await supabase
+        .from('Scripts')
+        .update([
+          {
+            script: scenesData,
+          }
+        ])
+        .eq('id', currentScriptId);
+    }else{
+      await supabase
+        .from('Scripts')
+        .insert([
+          {
+            uid: user?.id,
+            script: scenesData,
+            script_name: file?.name
+          }
+        ]);
+    }
+    alert("Congrats Your Script has Been Saved Successfully");  
+  }
+
+
+  const loadSavedScript = async (script_id: number) => {
+    const {data, error} = await supabase
+      .from('Scripts')
+      .select('script')
+      .eq('id', script_id)
+      .single(); // needed when we need one entry
+    
+      if (!error) {
+        setCurrentScriptId(script_id);
+        setScenesData(data.script); // Worked After i changed the type to jsonb
+        setLoading(false);
+      };
+  }
+
 
   const addScene = () => {
-    setData(prev => [
+    setScenesData(prev => [
       {
         id: Date.now(),                // unique key
         scene_number: prev.length + 1, // auto increment
@@ -168,7 +221,7 @@ export default function EditableTable() {
   };
 
   const deleteScene = (scene_to_remove: number) => {
-    setData(prev  => prev.filter(scene => scene.scene_number != scene_to_remove))
+    setScenesData(prev  => prev.filter(scene => scene.scene_number != scene_to_remove))
   }
 
   // When Script is Uploaded
@@ -177,6 +230,7 @@ export default function EditableTable() {
     const selectedFile = e.target.files[0];
     setFile(selectedFile);
     setLoading(true);
+    setCurrentScriptId(null); // when new file set currentScriptId to null
 
     const formData = new FormData();
     formData.append("script", selectedFile); // key must match server-side multer field
@@ -187,14 +241,16 @@ export default function EditableTable() {
     })
     .then(res => res.json())
     .then(data => {
-      setData(data.scenesData);
+      setScenesData(data.scenesData);
       setLoading(false);
+      setCurrentScriptId(null); // Set to null here again in case someone opened an existing script during loading
     })
     .catch(err => console.error(err));
   };
 
+
   const orderScenes = () => {
-    setData(prev =>
+    setScenesData(prev =>
       [...prev].sort((a, b) => a.scene_number - b.scene_number)
     );
   };
@@ -207,7 +263,7 @@ export default function EditableTable() {
       <h1 className="text-2xl font-semibold mb-4">Scene Breakdown</h1>
 
       {/* Uploading The files  - Need a fix in here as the input is only take from a very small area */}
-      <div className="relative text-center border-2 border-dashed border-gray-300 rounded-lg mb-6 p-6 cursor-pointer hover:border-blue-400 transition">
+      <div className="relative text-center border-2 border-dashed border-gray-300 rounded-lg mb-3 p-6 cursor-pointer hover:border-blue-400 transition">
         <input
           type="file"
           accept=".pdf"
@@ -217,9 +273,34 @@ export default function EditableTable() {
         {!file ? (
           <p className="text-gray-500">Click or Drag your script here to Upload</p>
         ) : (
-          <p className="text-gray-700">{file.name}</p>
+          <>
+            <p className="text-gray-700">{file.name}</p>
+            <p className="text-gray-700 underline">Choose Another File</p>
+          </>
         )}
       </div>
+
+
+      {/* View Saved Scripts */}
+      {user && (
+        <>
+          <h2 className="font-semibold mb-2">Select a Saved Script</h2>
+          <select
+            defaultValue=""
+            className="w-full border rounded px-2 py-1 mb-5"
+            onChange={(e) => loadSavedScript(Number(e.target.value))}
+          >
+            <option value="" disabled>Select a script</option>
+
+            {savedScripts.map(script => (
+              <option key={script.id} value={script.id}>
+                {script.script_name}
+              </option>
+            ))}
+          </select>        
+        </>
+      )}
+
 
       {/* Loader to Appear when data is not yet available */}
       {file && loading && (
@@ -239,10 +320,12 @@ export default function EditableTable() {
 
 
       {/* Display the Generate Schedule Button */}
-      {file && !loading && (
+      {!loading && (
         <>
           <div className="flex justify-between gap-2">
-            <button className="bg-green-400 hover:bg-green-500 rounded-2xl w-full mb-6 p-3 text-white font-bold cursor-pointer">Save Scenes</button>
+            <button className="bg-green-400 hover:bg-green-500 rounded-2xl w-full mb-6 p-3 text-white font-bold cursor-pointer"
+            onClick={saveScript}
+            >{currentScriptId ? "Update Script": "Save Script"}</button>
             <button className="bg-yellow-400 hover:bg-yellow-500 rounded-2xl w-full mb-6 p-3 text-white font-bold cursor-pointer"
             onClick={addScene}
             >Add Scene</button>
@@ -290,7 +373,7 @@ export default function EditableTable() {
 
 
       {/* Editing The Extracted Contents from Scenes */}
-      {!loading && (
+      {user && !loading && (
         <>
 
           <div className="overflow-x-auto rounded-lg border border-gray-300 bg-white shadow">
@@ -329,7 +412,7 @@ export default function EditableTable() {
               </thead>
 
               <tbody>
-                {data.map((row,idx) => (
+                {scenesData.map((row,idx) => (
                   <tr
                     key={idx}
                     className="odd:bg-white even:bg-gray-50 hover:bg-blue-50 transition"
